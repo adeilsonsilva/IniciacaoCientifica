5d23a37a05b6414d8bce677001fe7c09c32df0ac
TheusStremens <matheus.beep@gmail.com>
2015-09-15 16:51:27 -0300

    tempo para jogar e desabilitar as atividades apos conclusao

--- Atividades de Captura/random.js
+++ Atividades de Captura/random.js
@@ -5,6 +5,11 @@ function sortearAtvidades() {
         clearTimeout(myModal.data('hideInterval'));
         myModal.data('hideInterval', setTimeout(function(){
             myModal.modal('hide');
+            var x = document.getElementsByClassName("portfolio-item");
+            for(var i = 0; i < x.length; i++) {
+				if(x[i].getElementsByTagName("a")[0].href.split("#")[1].toString() == "portfolioModal3" )
+					x[i].setAttribute("style", "opacity: 0.5; pointer-events: none; cursor: default;");
+			}
         }, 180000));
     });
 	});
@@ -14,6 +19,11 @@ function sortearAtvidades() {
         clearTimeout(myModal.data('hideInterval'));
         myModal.data('hideInterval', setTimeout(function(){
             myModal.modal('hide');
+            var x = document.getElementsByClassName("portfolio-item");
+            for(var i = 0; i < x.length; i++) {
+				if(x[i].getElementsByTagName("a")[0].href.split("#")[1].toString() == "portfolioModal4" )
+					x[i].setAttribute("style", "opacity: 0.5; pointer-events: none; cursor: default;");
+			}
         }, 180000));
     });
 	});
@@ -31,7 +41,6 @@ function sortearAtvidades() {
 		if(!found)
 			arr[arr.length]=randomnumber;
 		}
-	//console.log(arr);
 	var portfolio = "#portfolioModal"
 	var x = arr[0].toString();
 	var dest = portfolio.concat(x);
@@ -44,5 +53,14 @@ function sortearAtvidades() {
     x = arr[2].toString();
     dest = portfolio.concat(x);
     document.getElementById("atividade3").href=dest; 
+}
+
+function desativarAtividade(portfolioModal1) {
+	var x = document.getElementsByClassName("portfolio-item");
+	//console.log(x);
+	for(var i = 0; i < x.length; i++) {
+		if(x[i].getElementsByTagName("a")[0].href.split("#")[1].toString() == portfolioModal1.toString() )
+			x[i].setAttribute("style", "opacity: 0.5; pointer-events: none; cursor: default;");
+	}
 }
 
